8390021975c6ad0f3999be0e179904940111df06
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-11-21 13:45:56 +0530

    inital setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "@/App";
-import "@/index.css";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { api } from "@/state/api";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-export const store = configureStore({
-  reducer: { [api.reducerPath]: api.reducer },
-  middleware: (getDefault) => getDefault().concat(api.middleware),
-});
-setupListeners(store.dispatch);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </Provider>
-);
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
