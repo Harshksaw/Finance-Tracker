f31971e7e6ab4ed1d7a170015f023ea6250d282d
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-09 14:03:39 +0000

    Layout React Query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,33 @@
 // Import the necessary dependencies from the 'react' and 'react-dom' libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // Import the CSS file for styling
-import './index.css';
+import '@/index.css';
 
 // Import the main App component from '@/App'
 import App from '@/App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import {api} from "@/state/api";
 
+export const store = configureStore({
+  reducer: {[ api.reducerPath]: api.reducer},
+  middleware: (getDefault)=> getDefault().concat(api.middleware),
+
+})
+
+setupListeners(store.dispatch)
 // Render the App component within the 'root' element in the HTML document
-ReactDOM.render(
-  // Wrap the App component with <React.StrictMode> for additional checks in development mode
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+
+
+  <Provider store={store}>
+
     <App />
-  </React.StrictMode>,
+  </Provider>
+
   // Find the HTML element with the id 'root' and render the App component inside it
-  document.getElementById('root')
+
 );
--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Dashboard from '.'
 import DashBoardBox from '@/components/DashBoardBox'
+import { useGetKpisQuery } from '@/state/api'
 
 type Props = {}
 
 const Row1 = (props: Props) => {
+  const{data} = useGetKpisQuery();
+
+  
   return (
     <>
     <DashBoardBox gridArea="a"></DashBoardBox>
